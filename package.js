4bd03a3fd4bd33edfadaff3bdca9c7e5c903b8aa
javiergarciagonzalez <javier.garciagonzalez@bq.com>
2016-05-09 17:06:25 +0200

    Update package.js

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
-  api.use('ecmascript', 'bquarks:aerial-mongo');
+  api.use(['ecmascript', 'bquarks:aerial-mongo']);
   api.mainModule('aerialjs.js');
 });
 
