c142c3bfd2e75e03f0c50a2453f8f0a85b7c3ecf
Francisco López <donflopez@gmail.com>
2016-07-06 17:30:00 +0200

    Update versions

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bquarks:aerialjs',
-    version: '0.1.5',
+    version: '0.1.6',
     // Brief, one-line summary of the package.
     summary: 'Suite Aerialjs to connect Meteor applications with Corble Platform.',
     // URL to the Git repository containing the source code for this package.
@@ -8,15 +8,15 @@ Package.describe({
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
-});
+  });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
     api.versionsFrom('METEOR@1.3.2.4');
-    api.use(['ecmascript', 'bquarks:aerial-mongo@0.0.15', 'bquarks:aerial-minimongo@0.0.19',
-              'bquarks:aerial-corbel@0.2.11', 'bquarks:aerial-accounts@0.5.0'], { weak: false, unordered:false });
+    api.use(['ecmascript', 'bquarks:aerial-mongo@0.0.16', 'bquarks:aerial-minimongo@0.0.23',
+              'bquarks:aerial-corbel@0.2.18', 'bquarks:aerial-accounts@0.5.0'], { weak: false, unordered:false });
 
     api.mainModule('aerialjs.js');
-});
+  });
 
 // Package.onTest(function(api) {
 //   api.use('ecmascript');
